59eab08b0bab5f6a8e40c744ed65b366cf9135f1
mhdix <domanloom@gmail.com>
2025-01-01 02:13:02 -0800

    feature: delete post item 20

--- A/home/urls.py
+++ A/home/urls.py
@@ -5,5 +5,7 @@ app_name='home'
 urlpatterns = [
     path('', views.HomeView.as_view() , name='home'),
     path('post/', views.PostView.as_view(), name="posts"),
+    # path('user/<iny:user_id>', views.UserProfile.as_view(), name="user_profile"),
     path('post/<int:post_id>/<slug:post_slug>/', views.DetailView.as_view(), name="posts-detail"),
+    path('delete/<int:post_id>/', views.DeletePostView.as_view(), name="post_delete"),
 ]
--- A/home/views.py
+++ A/home/views.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.forms import User
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, req):
         return render(req, 'home/index.html')
-
-
 
 class PostView(View):
     def get(self, req):
@@ -18,14 +18,21 @@ class PostView(View):
     
     def post(self, req):
         return render(req, 'home/index.html')
-    
-# class PostDetailView(View):
-#     def get(self, req):
-#         return render(req, 'home/detail.html')
-
 
 class DetailView(View):
     def get(self, req, post_id, post_slug):
         post = Post.objects.get(id=post_id, slug=post_slug)
         return render(req, 'home/detail.html', {'post': post})
         
+
+class DeletePostView(LoginRequiredMixin, View):
+    def get(self, req, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == req.user.id:
+            post.delete()
+            messages.success(req, 'deleted item successfuly', 'success')
+        else:
+            messages.error(req, 'you cant delete this post', 'error')
+        return redirect('home:home')
+
+
